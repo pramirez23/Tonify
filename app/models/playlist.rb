74b895dc09e28c561685f20d1ea6eeb5e64368b8
Paul Ramirez <pramirez7230@bths.edu>
2021-01-24 19:35:55 -0500

    Fix typo in Playlist model

--- app/models/playlist.rb
+++ app/models/playlist.rb
@@ -7,7 +7,7 @@ class Playlist < ApplicationRecord
     foreign_key: :user_id,
     class_name: :User
 
-  has many :playlist_songs,
+  has_many :playlist_songs,
     primary_key: :id,
     foreign_key: :playlist_id,
     class_name: :Playlist,
