17e2fbf13e8c36d86abc96e75a80ebd4ea4f88e5
Paul Ramirez <pramirez7230@bths.edu>
2021-01-24 19:33:16 -0500

    Update syntax error in like model

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,5 +1,5 @@
 class Like < ApplicationRecord
   validates :user_id, :likable_id, :likable_type, presence: true
-  validates :user_id, uniqueness: { scope: :likable_id, :likable_type }
+  validates :user_id, uniqueness: { scope: [:likable_id, :likable_type] }
   belongs_to :likable, polymorphic: true
 end
