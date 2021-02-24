03852ccb6f6eeba808af0d7c23c4de2302dcd217
Paul Ramirez <pramirez7230@bths.edu>
2021-02-23 19:32:34 -0500

    Change song list item number to play button on hover

--- app/assets/javascripts/bundle.js
+++ app/assets/javascripts/bundle.js
@@ -1564,26 +1564,109 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony export */   "default": () => __WEBPACK_DEFAULT_EXPORT__
 /* harmony export */ });
 /* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");
+function _typeof(obj) { "@babel/helpers - typeof"; if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
+
+function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function"); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, writable: true, configurable: true } }); if (superClass) _setPrototypeOf(subClass, superClass); }
+
+function _setPrototypeOf(o, p) { _setPrototypeOf = Object.setPrototypeOf || function _setPrototypeOf(o, p) { o.__proto__ = p; return o; }; return _setPrototypeOf(o, p); }
+
+function _createSuper(Derived) { var hasNativeReflectConstruct = _isNativeReflectConstruct(); return function _createSuperInternal() { var Super = _getPrototypeOf(Derived), result; if (hasNativeReflectConstruct) { var NewTarget = _getPrototypeOf(this).constructor; result = Reflect.construct(Super, arguments, NewTarget); } else { result = Super.apply(this, arguments); } return _possibleConstructorReturn(this, result); }; }
+
+function _possibleConstructorReturn(self, call) { if (call && (_typeof(call) === "object" || typeof call === "function")) { return call; } return _assertThisInitialized(self); }
+
+function _assertThisInitialized(self) { if (self === void 0) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return self; }
+
+function _isNativeReflectConstruct() { if (typeof Reflect === "undefined" || !Reflect.construct) return false; if (Reflect.construct.sham) return false; if (typeof Proxy === "function") return true; try { Date.prototype.toString.call(Reflect.construct(Date, [], function () {})); return true; } catch (e) { return false; } }
+
+function _getPrototypeOf(o) { _getPrototypeOf = Object.setPrototypeOf ? Object.getPrototypeOf : function _getPrototypeOf(o) { return o.__proto__ || Object.getPrototypeOf(o); }; return _getPrototypeOf(o); }
 
 
 
-var SongListItem = function SongListItem(_ref) {
-  var song = _ref.song,
-      num = _ref.num;
-  return /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("tr", {
-    className: "song"
-  }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("td", {
-    className: "num-column"
-  }, num), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("td", {
-    className: "title-column"
-  }, song.title, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("br", null), "\"this is the artist's name\""), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("td", {
-    className: "album-column"
-  }, "Album title placeholder"), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("td", {
-    className: "date-added-column"
-  }, "Date added here"), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("td", {
-    className: "duration-column"
-  }, song.duration));
-};
+
+var SongListItem = /*#__PURE__*/function (_React$Component) {
+  _inherits(SongListItem, _React$Component);
+
+  var _super = _createSuper(SongListItem);
+
+  function SongListItem(props) {
+    var _this;
+
+    _classCallCheck(this, SongListItem);
+
+    _this = _super.call(this, props);
+    _this.state = {
+      isHovering: false
+    };
+    _this.handleHover = _this.handleHover.bind(_assertThisInitialized(_this));
+    return _this;
+  }
+
+  _createClass(SongListItem, [{
+    key: "handleHover",
+    value: function handleHover() {
+      this.setState({
+        isHovering: !this.state.isHovering
+      });
+    }
+  }, {
+    key: "render",
+    value: function render() {
+      var _this2 = this;
+
+      var song = this.props.song;
+      var isHovering = this.state.isHovering;
+      var playOrNum;
+
+      if (isHovering) {
+        playOrNum = /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("i", {
+          className: "fas fa-play"
+        });
+      } else {
+        playOrNum = this.props.num;
+      }
+
+      return /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("tr", {
+        className: "song",
+        onMouseEnter: function onMouseEnter() {
+          return _this2.handleHover();
+        },
+        onMouseLeave: function onMouseLeave() {
+          return _this2.handleHover();
+        }
+      }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("td", {
+        className: "num-column"
+      }, playOrNum), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("td", {
+        className: "title-column"
+      }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("div", {
+        className: "title-details"
+      }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("div", {
+        className: "item-art-container"
+      }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("img", {
+        className: "item-album-art",
+        src: song.cover_art,
+        alt: "cover art"
+      })), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("div", {
+        className: "title-artist-container"
+      }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("p", {
+        className: "song-title"
+      }, song.title), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("p", null, "\"this is the artist's name\"")))), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("td", {
+        className: "album-column"
+      }, "Album title placeholder"), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("td", {
+        className: "date-added-column"
+      }, "Date added here"), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("td", {
+        className: "duration-column"
+      }, song.duration));
+    }
+  }]);
+
+  return SongListItem;
+}(react__WEBPACK_IMPORTED_MODULE_0__.Component);
 
 /* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (SongListItem);
 
--- frontend/components/songs/song_list_item.jsx
+++ frontend/components/songs/song_list_item.jsx
@@ -1,24 +1,59 @@
 import React from 'react';
 import { Link } from 'react-router-dom'; 
 
-const SongListItem = ({ song, num }) => {
-    return(
-      <tr className="song">
-        <td className="num-column">{num}</td>
+class SongListItem extends React.Component {
+  constructor(props) {
+    super(props);
+    
+    this.state = {
+      isHovering: false
+    };
+    
+    this.handleHover = this.handleHover.bind(this);
+  }
 
-        <td className="title-column">
-          {song.title}
-          <br/>
-          "this is the artist's name"
+  handleHover() {
+    this.setState({
+      isHovering: !this.state.isHovering
+    });
+  }
+
+  render() {
+    const song = this.props.song;
+    const isHovering = this.state.isHovering;
+
+    let playOrNum;
+
+    if (isHovering) {
+      playOrNum = <i className="fas fa-play"></i>;
+    } else {
+      playOrNum = this.props.num;
+    }
+
+    return (
+      <tr
+        className="song"
+        onMouseEnter={() => this.handleHover()}
+        onMouseLeave={() => this.handleHover()}
+      >
+        <td className="num-column">{playOrNum}</td>
+        <td className="title-column"> 
+          <div className="title-details">
+            <div className="item-art-container">
+              <img className="item-album-art" src={song.cover_art} alt="cover art" />
+            </div>
+            <div className="title-artist-container">
+              <p className="song-title">{song.title}</p>
+              <p>"this is the artist's name"</p>
+            </div>
+          </div>
         </td>
-
         <td className="album-column">Album title placeholder</td>
-
         <td className="date-added-column">Date added here</td>
-
         <td className="duration-column">{song.duration}</td>
       </tr>
     )
-};
+  }
+}
 
 export default SongListItem;
